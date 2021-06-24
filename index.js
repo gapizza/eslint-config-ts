ba5ee0d15cea5f8a656daaf0b68ca1d46415a133
Eric Hacke <eric@gapizza.com>
2021-06-24 09:57:39 -0400

    feat: update rules
    BREAKING CHANGE: new erros

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ module.exports = {
   parser: '@typescript-eslint/parser',
   plugins: ['@typescript-eslint', 'import', 'simple-import-sort'],
   rules: {
+    '@typescript-eslint/explicit-module-boundary-types': 'error',
     '@typescript-eslint/no-explicit-any': 'off',
     '@typescript-eslint/no-shadow': ['error'],
     '@typescript-eslint/no-unused-vars': ['error', { args: 'after-used', ignoreRestSiblings: true, vars: 'all' }],
